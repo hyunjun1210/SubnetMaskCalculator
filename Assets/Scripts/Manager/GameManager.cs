8722d76969624d91cf8972ed3a15d6faa48760a3
현준 <117495886+hyunjun1210@users.noreply.github.com>
2026-02-05 11:02:34 +0900

    Update

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -6,9 +6,21 @@ using static UnityEngine.EventSystems.EventTrigger;
 
 public class GameManager : MonoBehaviour
 {
-
     private static GameManager m_instance = null;
     public static GameManager Instance => m_instance;
+
+    [DllImport("user32.dll")]
+    static extern IntPtr GetActiveWindow();
+
+    [DllImport("user32.dll")]
+    static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
+
+    [DllImport("user32.dll")]
+    static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
+
+    const int GWL_STYLE = -16;
+    const uint WS_POPUP = 0x80000000;
+    const uint WS_VISIBLE = 0x10000000;
 
     private void Awake()
     {
@@ -28,23 +40,4 @@ public class GameManager : MonoBehaviour
         SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
 #endif
     }
-
-    [DllImport("user32.dll")]
-    static extern IntPtr GetActiveWindow();
-
-    [DllImport("user32.dll")]
-    static extern int SetWindowLong(IntPtr hWnd, int nIndex, uint dwNewLong);
-
-    [DllImport("user32.dll")]
-    static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
-
-    [DllImport("user32.dll")]
-    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
-
-    const int GWL_STYLE = -16;
-    const uint WS_POPUP = 0x80000000;
-    const uint WS_VISIBLE = 0x10000000;
-
-    const uint SWP_SHOWWINDOW = 0x0040;
-    const uint SWP_FRAMECHANGED = 0x0020;
 }
