a470f5ccfa2f49f04756cab5f04f8b60c1feb54b
현준 <117495886+hyunjun1210@users.noreply.github.com>
2026-02-05 10:54:31 +0900

    고침

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -1,6 +1,8 @@
-using System.Runtime.InteropServices;
 using System;
+using System.Runtime.InteropServices;
 using UnityEngine;
+using UnityEngine.XR;
+using static UnityEngine.EventSystems.EventTrigger;
 
 public class GameManager : MonoBehaviour
 {
@@ -19,6 +21,12 @@ public class GameManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+
+#if !UNITY_EDITOR
+        Screen.SetResolution(800, 900, false);
+        IntPtr hwnd = GetActiveWindow();
+        SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
+#endif
     }
 
     [DllImport("user32.dll")]
@@ -30,16 +38,13 @@ public class GameManager : MonoBehaviour
     [DllImport("user32.dll")]
     static extern uint GetWindowLong(IntPtr hWnd, int nIndex);
 
+    [DllImport("user32.dll")]
+    static extern bool SetWindowPos(IntPtr hWnd, IntPtr hWndInsertAfter, int X, int Y, int cx, int cy, uint uFlags);
+
     const int GWL_STYLE = -16;
     const uint WS_POPUP = 0x80000000;
     const uint WS_VISIBLE = 0x10000000;
 
-    void Start()
-    {
-#if !UNITY_EDITOR
-        Screen.SetResolution(800, 900, false);
-        IntPtr hwnd = GetActiveWindow();
-        SetWindowLong(hwnd, GWL_STYLE, WS_POPUP | WS_VISIBLE);
-#endif
-    }
+    const uint SWP_SHOWWINDOW = 0x0040;
+    const uint SWP_FRAMECHANGED = 0x0020;
 }
